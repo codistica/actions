6a3a100dd7d8884e64cd61c192f0b07fd77fe58a
Javier Monguilo <javier.monguilo@codistica.com>
2020-05-01 21:02:17 +0200

    [NEW] - Add monorepo-release-tag action.

---
+++ monorepo-release-tags/index.js
@@ -0,0 +1,108 @@
+const cp = require('child_process');
+const path = require('path');
+
+cp.execSync(`cd ${__dirname}; yarn install`);
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+/**
+ * @description Generates a Codistica date release tag.
+ * @returns {string} - Tag in format YYYY/MM/DD.
+ */
+const getDateTag = () => {
+    const today = new Date();
+
+    /**
+     * @description Puts 0 to the left
+     * @param {number} num - Input number.
+     * @param {number} places - Final length.
+     * @returns {string} Number with zeroes on the left.
+     */
+    const zeroPad = (num, places) => String(num).padStart(places, '0');
+    return(
+        today.getFullYear().toString() + '/' +
+        zeroPad(today.getMonth()+1, 2) + '/' +
+        zeroPad(today.getDate(), 2)
+    );
+};
+
+(async () => {
+
+    const token = core.getInput("token", { required: true });
+    const monorepoName = core.getInput("monorepo-name", { required: true });
+    const checkOnly = core.getInput("check-only", { required: false }) || false;
+
+    if (!process.env.GITHUB_SHA) {
+        core.setFailed('ERROR: No env.GITHUB_SHA present')
+    }
+
+    const oct = new github.GitHub(token);
+
+    const changedPackages = cp.execSync('yarn run --silent lerna list').toString().trim().split('\n');
+
+    let tags = [];
+    let exposeTags = [];
+
+    changedPackages.forEach((changedPackage) => {
+        const jsonPath = path.resolve(
+            './packages',
+            changedPackage.replace(`${monorepoName}/`, ''),
+            'package.json'
+        );
+        tags.push(`${changedPackage}@${require(jsonPath).version}`)
+    });
+
+    cp.execSync('git fetch --tags');
+    const existingTags = cp.execSync('git tag').toString().trim().split('\n');
+
+    if(checkOnly) core.info('Action will not generate a release.');
+
+    for (const tag of tags) {
+
+        // CHECK IF TAG ALREADY EXISTS
+        if(existingTags.includes(tag)) continue;
+
+        // PUSH TO EXPOSED OUTPUT
+        exposeTags.push(tag);
+
+        if(checkOnly) continue;
+
+        await oct.repos.createRelease({
+            ...github.context.repo,
+            tag_name: tag,
+            target_commitish: process.env.GITHUB_SHA,
+            name: `${tag}`,
+            draft: false,
+        });
+
+        core.info(`Tag created: ${tag}`)
+    }
+
+    if (exposeTags.length === 0) {
+        core.info('No new tags.');
+        exposeTags = '0'
+    } else {
+        const dateTag = getDateTag();
+        if (!existingTags.includes(dateTag)){
+            exposeTags.push(dateTag);
+            if(!checkOnly) {
+                await oct.repos.createRelease({
+                    ...github.context.repo,
+                    tag_name: dateTag,
+                    target_commitish: process.env.GITHUB_SHA,
+                    name: `Release of ${dateTag}`,
+                    draft: false,
+                });
+            }
+        } else {
+            core.info(`Tag ${dateTag} already exists.`)
+        }
+    }
+
+    core.setOutput('tags', JSON.stringify(exposeTags));
+})().catch(
+    (err) => {
+        core.setFailed(err.message)
+    }
+);
